9a78844a2fe259f0b07d9c59d9af60df6fb6cf3e
adricatena <adricatena@gmail.com>
2021-01-28 13:26:27 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,70 @@
+var encabezado = `
+    <div>
+    <h1>Cucina di Tati</h1>
+    </div>
+    `
+var nav = `
+    <a class="navbar-brand" href="index.html">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">Sobre nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="productos.html">Nuestros productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.html">Registro</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sucursales.html">Sucursales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contacto</a>
+                    </li>
+                </ul>
+        </div>`
+var pie = `
+    <div>
+    <a href="https://www.instagram.com/cucinaditati/" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.facebook.com/arai.moreyra.1" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    </div>
+    <p>Derechos reservados @2020</p>`
+document.getElementById("idHeader").innerHTML = encabezado;
+document.getElementById("idNav").innerHTML = nav;
+document.getElementById("idFooter").innerHTML = pie;
+
+if (document.getElementById("idCarrousel")) {
+    var producto = `
+    <div class="col-12">
+        <div id="demo" class="carousel slide pt-1" data-ride="carousel">
+            <div class="carousel-inner">`;
+    for (var i = 0; i < productos.length; i++) {
+        producto += `<div class="carousel-item`;
+        if (i == 0)
+            producto += ` active">`;
+        else
+            producto += `">`;
+        producto += `
+            <img class="img-fluid rounded" src="${productos[i].src}" alt="${productos[i].alt}">
+            <h4>${productos[i].titulo}</h4>
+            </div>`;
+    }
+    producto += `
+            </div>
+            <a class="carousel-control-prev" href="#demo" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#demo" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>`;
+    document.getElementById("idCarrousel").innerHTML = producto;
+};
---
+++ js/productos.js
@@ -0,0 +1,62 @@
+var productos = [
+    {
+        codigo: "cod1",
+        src: "img/alfajores-maicena.jpeg",
+        alt: "alfajores de maicena",
+        titulo: "Alfajorcitos de Maicena"
+    },
+    {
+        codigo: "cod2",
+        src: "img/cheesecake-frutos-rojos.jpeg",
+        alt: "cheesecake de frutos rojos",
+        titulo: "Cheesecake de Frutos Rojos"
+    },
+    {
+        codigo: "cod3",
+        src: "img/cuadraditos-frutilla.jpeg",
+        alt: "cuadraditos de frutilla",
+        titulo: "Cuadraditos de Frutilla"
+    },
+    {
+        codigo: "cod4",
+        src: "img/letter-cake.jpeg",
+        alt: "letter cake",
+        titulo: "Letter cake"
+    },
+    {
+        codigo: "cod5",
+        src: "img/matilda-dulces.jpeg",
+        alt: "matilda con dulces",
+        titulo: "Matilda con dulces"
+    },
+    {
+        codigo: "cod6",
+        src: "img/matilda-frutos-rojos.jpeg",
+        alt: "matilda con frutos rojos",
+        titulo: "Matilda con Frutos Rojos"
+    },
+    {
+        codigo: "cod7",
+        src: "img/mini-lemonpie.jpeg",
+        alt: "minis lemon pie",
+        titulo: "Minis Lemon Pie"
+    },
+    {
+        codigo: "cod8",
+        src: "img/torta-circo.jpg",
+        alt: "torta de circo",
+        titulo: "Torta decorada de Circo"
+    },
+    {
+        codigo: "cod9",
+        src: "img/torta-coco.jpeg",
+        alt: "tarta de coco",
+        titulo: "Tarta de Coco"
+    },
+    {
+        codigo: "cod10",
+        src: "img/torta-friends.jpg",
+        alt: "torta de friends",
+        titulo: "Torta decorada de Friends"
+    }
+];
